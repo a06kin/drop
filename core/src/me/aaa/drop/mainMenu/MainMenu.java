bd26201792d1a5d851c87e85b4c73ce1d3eba716
Aleksandr Anohin <a06kin@gmail.com>
2015-04-19 19:53:56 +0300

    game class main menu

---
+++ core/src/me/aaa/drop/mainMenu/MainMenu.java
@@ -0,0 +1,48 @@
+package me.aaa.drop.mainMenu;
+
+
+import com.badlogic.gdx.Screen;
+import me.aaa.drop.Drop;
+
+public class MainMenu implements Screen {
+
+
+    public MainMenu(Drop drop) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
