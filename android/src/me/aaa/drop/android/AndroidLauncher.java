fa1d468192991ba086b9154c5444c56687a342cd
Aleksandr Anohin <a06kin@gmail.com>
2015-04-21 22:33:22 +0300

    game class name change

--- android/src/me/aaa/drop/android/AndroidLauncher.java
+++ android/src/me/aaa/drop/android/AndroidLauncher.java
@@ -1,16 +1,15 @@
 package me.aaa.drop.android;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import me.aaa.drop.Drop;
+import me.aaa.drop.DropGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Drop(), config);
+		initialize(new DropGame(), config);
 	}
 }
